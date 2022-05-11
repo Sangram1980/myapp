0cac5e64eb05ff47237dd8945b94a68701de6cbe
Sangram1980 <beherasangram@gmail.com>
2022-05-11 05:18:01 -0700

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
     return (
         <>
-            <h1 className="text-center">Welcome To Elevate Buy</h1>
+            <h1 className="text-center">Welcome To Elevate Buy, BBSR</h1>
             <div className="container">
                 <div className="row">
                     <div className="col">
@@ -13,7 +13,7 @@ const App = () => {
                             <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height={'300px'}/>
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <p className="card-text">Elevate Buy</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
